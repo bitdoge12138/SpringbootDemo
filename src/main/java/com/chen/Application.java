cf8bdbf062bdd22c1868f5bbde4c61c09a665f54
chen <2224570705@qq.com>
2023-09-26 21:13:38 +0800

    搭建Springboot增删改查基本骨架

---
+++ src/main/java/com/chen/Application.java
@@ -0,0 +1,13 @@
+package com.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Spring项目启动成功");
+    }
+}
---
+++ src/main/java/com/chen/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.chen.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.chen.mapper.UserMapper;
+import com.chen.pojo.User;
+import com.chen.service.UserService;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+}
